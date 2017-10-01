e6276ae5667e7711ed06d46de8aa8cd830c5bcb4
Ganeshmahesh <ganeshmmahesh1992@gmail.com>
2017-10-01 15:24:11 -0500

    memory and disk perf calc code added

--- Disk/disk_benchmark.c
+++ Disk/disk_benchmark.c
@@ -10,71 +10,108 @@
 #define BILLION 1000000000L
 #define INSTR 9
 #define MEMORY_CHUNK_SIZE 1000000000  //1GB
+#define EXP_DURATION 20 //20s 
 
-long` get_total_op(int no_threads)
+int *thread_op_array; //array holding total operations completed by each thread
+char *targer_mem_ptr;
+char *source_mem_ptr;
+
+struct thread_data_t
+{
+  int block_size;
+  int from;
+  int to;
+} *thread_data;
+
+long get_total_op(int no_threads)
 {
   long total_op = 0;
   int j;
-  for(j = 0; j < no_threads; J++)
+  for(j = 0; j < no_threads; j++)
   {
-    total_op += thread_op_array[i];
+    total_op += thread_op_array[j];
   }
   return total_op;
 }
 
-int *thread_op_array; //array holding total operations completed by each thread
-char *mem_ptr;
-
-void allocate_memory_chunks(int no_threads)
-{
-  int i;
-  
-  mem_ptr = calloc(no_threads,sizof(char));
-  for(i = 0;i < no_threads; i++)
-  {
-    mem_ptr[i] = (char *) malloc(sizeof(char)*MEMORY_CHUNK_SIZE);
-  }
-
-}
+void 
+exec_threads (void *method, int no_threads, int block_size);
 
 void*
 read_write_op(void * param);
+
 void*
 seq_write_op(void* param);
+
 void*
 rand_write_op(void* param);
 
 void*
 read_write_op(void * param)
 {
-}
-void*
-seq_write_op(void* param)
-{
-}
-void*
-rand_write_op(void* param)
-{
   
+  int thread_id = *((int *)param);
+  struct timespec start;
+
+  while(start.tv_sec < start.tv_sec + EXP_DURATION)
+  {
+   // memcpy(target_mem_p,tr,source_mem_ptr,thread_data[thread_id]->block_size);
+    clock_gettime(CLOCK_REALTIME,&start);
+    thread_op_array[thread_id]++; 
+  }
 }
 
-void calculate_mem_perf(void (*method)(void *),int no_threads, int block_size)
+void*
+seq_write_op(void* param)
+{ 
+  int thread_id = *((int *)param);
+  struct timespec start;
+
+  while(start.tv_sec < start.tv_sec + EXP_DURATION)
+  {
+   // memcpy(target_mem_p,tr,source_mem_ptr,thread_data[thread_id]->block_size);
+    clock_gettime(CLOCK_REALTIME,&start);
+    thread_op_array[thread_id]++; 
+  }
+}
+
+void*
+rand_write_op(void* param)
+{ 
+  int thread_id = *((int *)param);
+  struct timespec start;
+
+  while(start.tv_sec < start.tv_sec + EXP_DURATION)
+  {
+   // memcpy(target_mem_p,tr,source_mem_ptr,thread_data[thread_id]->block_size);
+    clock_gettime(CLOCK_REALTIME,&start);
+    thread_op_array[thread_id]++; 
+  }
+} 
+
+void calculate_mem_perf(void* (*method)(void *),int no_threads, int block_size)
 {
   double latency, throughput;
   struct timespec start,end;
-
+  source_mem_ptr = (char *) malloc(sizeof(char) * block_size);
   clock_gettime(CLOCK_REALTIME,&start);
   exec_threads(method, no_threads, block_size);
   clock_gettime(CLOCK_REALTIME,&end);
+  double total_op = get_total_op(no_threads);
+  double total_sec = (double)(end.tv_nsec-start.tv_nsec/1000000)+(double)end.tv_sec-start.tv_sec;
+
+  throughput = ((total_op * (double) block_size) / 1048576) / (double) total_sec;
+  latency = (total_sec / (double) total_op) * 1000000;
+
 }
 
-void exec_threads (void *method, int no_threads)
+void exec_threads (void *method, int no_threads, int block_size)
 {
   pthread_t thread[8];
   int i;  
   for (i = 0; i < no_threads; i++)
   {
-    pthread_create (&thread[i], NULL, method, (void *)i);
+    pthread_create (&thread[i], NULL, method, (void *)&i);
   }  
 
   for (i = 0; i < no_threads; i++)
@@ -83,14 +120,25 @@ void exec_threads (void *method, int no_threads)
   }
 }
 
-void init_thread_data(int no_threads)
+void init_thread_data(int no_threads, int block_size)
 {
-  allocate_memory_chunks( no_threads); //initializing memory
+  int i=0;
+  int block_count;
+  targer_mem_ptr = (char *) malloc(MEMORY_CHUNK_SIZE);//initialize memory
+  thread_data = (struct thread_data_t *)malloc(sizeof(struct thread_data_t) * no_threads);
+  
+  block_count = MEMORY_CHUNK_SIZE / block_size;
+  for(i=0;i<no_threads;i++)
+  {
+    thread_data[i].block_size = block_size;
+    thread_data[i].from = i * (block_count / no_threads);
+    thread_data[i].to = ((block_count / no_threads) * (i+1))-1;
+  }
   thread_op_array= (int*) calloc(no_threads, sizeof(int));
 }
 
 //arg list param_space no_threada block_size
-int main(int argc, int *argv[])
+int main(int argc, char *argv[])
 {
   int no_threads = 1;// 1,2,4,8
   int param_space = 1; // 1 read/write , 2 sequential write, 3 random write
@@ -100,7 +148,7 @@ int main(int argc, int *argv[])
   param_space = atoi (argv[1]);
   block_size = atoi (argv[3]);
 
-  init_thread_data(no_threads);
+  init_thread_data(no_threads,block_size);
 
   if(param_space ==1)//read/write operation
   {
@@ -112,7 +160,7 @@ int main(int argc, int *argv[])
   }
   else//randome write
   {
-    calculate_mem_perf( ramd_write_op,no_threads, block_size);
+    calculate_mem_perf( rand_write_op,no_threads, block_size);
   }
   return 0; 
 }
