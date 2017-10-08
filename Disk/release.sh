827e84619a70433c5afa15382a389c3b9ff1def6
Ganeshmahesh <ganeshmmahesh1992@gmail.com>
2017-10-08 19:58:07 +0000

    disk result update

--- Disk/release.sh
+++ Disk/release.sh
@@ -10,20 +10,7 @@ for paramspace in {1,2,3}
 do
 	for no_threads in {1,2,4,8}
 	do
-		for block_size in {2,3,4} #8kb,8mb,80mb case, througput
-		do
-			echo "Parmeter space :$paramspace" , "Num of threads: $no_threads" , "Block Size: $block_size"
-			./disk_benchmark $paramspace $no_threads $block_size
-			echo "#################################################################################"
-		done
-	done	
-done
-
-for paramspace in {1,2,3}
-do
-	for no_threads in {1,2,4,8}
-	do
-		for block_size in {1} #8b case and latency
+		for block_size in {1,2,3,4} #8kb,8mb,80mb case, througput
 		do
 			echo "Parmeter space :$paramspace" , "Num of threads: $no_threads" , "Block Size: $block_size"
 			./disk_benchmark $paramspace $no_threads $block_size
