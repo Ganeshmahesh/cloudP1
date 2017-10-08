781ff633f73ada66a92525409b5f4a2ad29f8220
Ganeshmahesh <ganeshmmahesh1992@gmail.com>
2017-10-08 11:13:03 -0500

    adding multiplication subtraction and division opeations to cpu operation list

--- CPU/test.c
+++ CPU/test.c
@@ -51,7 +51,7 @@ compute_integer_op (void *param)
 
   while (i < ITERATIONS)
   {
-    k = k + a1+a2+a3+a4+a5+a6+a7+a8+a9+a10+a11+a12+a13+a14+a15+a16+a17
+    k = k + a1+a2+a3+a4-a5+a6+a7*a8+a9+a10+a11*a12+a13+a14/a15+a16+a17
           +a18+a19+a20+a21+a22+a23;
     i++;
   }
@@ -110,7 +110,7 @@ compute_float_op (void *param)
                         
   while (i < ITERATIONS)
   {
-    k = k + a1+a2+a3+a4+a5+a6+a7+a8+a9+a10+a11+a12+a13+a14+a15+a16+a17
+    k = k + a1+a2+a3+a4+a5+a6+a7-a8+a9+a10+a11+a12*a13*a14+a15/a16+a17
       +a18+a19+a20+a21+a22+a23;
     i++;
   }
