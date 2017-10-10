14648dd83aac2f93cbb4a18a9b52679d696caad1
Ganeshmahesh <ganeshmmahesh1992@gmail.com>
2017-10-10 03:42:11 -0500

    added changes to network

--- Network/server/network_benchmark_server.c
+++ Network/server/network_benchmark_server.c
@@ -6,28 +6,9 @@
 #include <string.h>
 #include <errno.h>
 #define ITERATIONS 1
-/*
-void exec_tcp_server(int packet_size,int no_threads)
-{
-  double latency, throughput;
-  struct timespec start,end;
+#define TRANSFER_SIZE 10737418240 //1GB
 
-  //clock_gettime(CLOCK_REALTIME,&start);
-  exec_threads(method, no_threads);
-  //clock_gettime(CLOCK_REALTIME,&end);
-
-  //double total_op = get_total_op(no_threads);
-  // // printf("%lf\n",total_op);
-  //double total_sec = (double)(end.tv_nsec-start.tv_nsec/1000000000)+(double)end.tv_sec-start.tv_sec;
-  //
-  //     //throughput = ((total_op * (double) block_size) / 1048576) / (double) total_sec;
-  //       //mesure in ms
-  //        // latency = (total_sec / (double) total_op) * 1000;
-  //          //printf("throughput %lf\n",throughput );
-  //            //printf("latency %lf\n",latency);
-   fprintf(stdout,"total sec %lf\n",(double)end.tv_sec-start.tv_sec);
-}
-*/
+long thread_operation_count;
 struct transmission_arguments
 {
   int sock;
@@ -45,17 +26,17 @@ void * udpOprtData(void *arg)
   int sockaddr_size = sizeof(struct sockaddr_in);
 //printf("%d",trans_arg->packet_size);
   int i = 0;
-  for (i = 0 ; i < ITERATIONS; ++i) 
+  for (i = 0 ; i < thread_operation_count; ++i) 
   {
     //:printf("server addr %ld\n",trans_arg->sock);
     int status =recvfrom(trans_arg -> sock, packet, trans_arg -> packet_size, 0, (struct sockaddr *)&client, &sockaddr_size);
     if(status == -1)
     {
       printf("error info %s",strerror(errno));
-      thread_exit(NULL);
+      pthread_exit(NULL);
     }
     else
-      //printf("%s oh yeah baby !\n",packet);
+    //  printf("%s oh yeah baby !\n",packet);
     //  printf("exiting");
     //
     memset(packet, 'z',sizeof(char)*trans_arg->packet_size);
@@ -132,13 +113,13 @@ void * tcpOprtData(void *arg)
   int sockaddr_size = sizeof(struct sockaddr_in);
 
   int i = 0;
-  for (i = 0 ; i < ITERATIONS; ++i)
+  for (i = 0 ; i < thread_operation_count; ++i)
   {
     int status = recv(trans_arg -> sock, packet, trans_arg -> packet_size, 0);
 
     if( status > 0)
     {
-     // printf("arrived packet %s oh yeah!\n",packet);
+      //printf("arrived packet %s oh yeah!\n",packet);
       memset(packet, 'g', sizeof(char) * trans_arg-> packet_size);
       send(trans_arg ->sock, packet, trans_arg->packet_size,0);
     }
@@ -238,7 +219,7 @@ int main(int argc, char *argv[])
    {
      packet_size = byte_val[packet_size - 1];
    }
-
+   thread_operation_count = TRANSFER_SIZE/packet_size;
    if( param_space == 1) //TCP stack
    {
      exec_tcp_server( packet_size, no_threads);
